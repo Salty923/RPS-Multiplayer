88ce0ca9e7e0ec246689a19dcb5dec71c2395897
Scott Saltzman <salty923@gmail.com>
2018-01-14 22:28:13 -0600

    Got mulitple two users with choices to database

--- assets/javascript/app3.js
+++ assets/javascript/app3.js
@@ -22,6 +22,29 @@ var firstPlayer = true;
 var totalPlayers;
 //create variable for database users
 var dbUsers = database.ref("users");
+//create db ref player1
+var dbplayer1 = database.ref("users/player1");
+// dbplayer1 choice
+var db1C = dbplayer1.child("pick");
+// dbplayer1 wins
+var db1W = dbplayer1.child("wins");
+// dbplayer1 losses
+var db1L = dbplayer1.child("losses");
+// dbplayer1 ties
+var db1T = dbplayer1.child("ties");
+//create db ref player1
+var dbplayer2 = database.ref("users/player2");
+// dbplayer1 choice
+var db2C = dbplayer2.child("pick");
+// dbplayer2 wins
+var db2W = dbplayer2.child("wins");
+// dbplayer2 losses
+var db2L = dbplayer2.child("losses");
+// dbplayer2 ties
+var db2T = dbplayer2.child("ties");
+//set this player
+var dbcurrent = 0;
+
 
 
 
@@ -37,44 +60,89 @@ $("#btnLogin").on("click",function () {
     var name = $("#username").val().trim();
     //add player name to UI
     $("#playerName").html(name);
-    //change the on value to once!  Thanks Megan!!
+    //changed the on value to once!  Thanks Megan!!
+    dbUsers.on("value",function (snpashot) {
+        if (totalPlayers >0) {
+            firstPlayer = false;
+        }
+    })
+
     //find total number of current users
     dbUsers.once("value", function (snpashot) { 
         totalPlayers = snpashot.numChildren();
         //Set players to database if room
         if (totalPlayers === 0) {
-                database.ref("users/player1").set({
+                dbplayer1.set({
                     username: name,
                     wins: 0,
                     losses: 0,
-                    pick: 0,
+                    ties:0,
+                    pick: "",
+                    message:"",
             })
+            dbcurrent = 1;
         } else if (totalPlayers === 1) {
             firstPlayer = false;
-            database.ref("users/player2").set({
+            dbplayer2.set({
                 username: name,
                 wins: 0,
                 losses: 0,
-                pick: 0,
+                ties:0,
+                pick: "",
+                message:"",
             })
+            dbcurrent = 2;
         } else {
             alert("please wait for game");
         }
     })
 });
 
-$("#r").on("click",function () {
-    alert("Rock");
+//Player One or two pick rock-set pick to r
+$("#r").on("click", function () {
+    if (dbcurrent === 1) {
+        db1C.set("r")
+    } else if (dbcurrent === 2) {
+        db2C.set("r")
+    }
+    gameLogic();
 });
 
+
+//Player One or two pick paper-set pick to p
 $("#p").on("click", function () {
-    alert("paper");
+    if (dbcurrent === 1) {
+        db1C.set("p")
+    } else if (dbcurrent === 2) {
+        db2C.set("p")
+    }
+    gameLogic();
 });
 
+//Player One or two pick scissors-set pick to s
 $("#s").on("click", function () {
-    alert("Scis");
+    if (dbcurrent === 1) {
+        db1C.set("s")
+    } else if (dbcurrent === 2) {
+        db2C.set("s")
+    }
+    gameLogic();
 });
 
+function gameLogic() {
+    if (((db1C === "r") && (db2C === "p")) || ((db1C === "p") && (db2C === "s")) || ((db1C === "s") && (db2C === "r"))) {
+        db1L++;
+        db2W++;
+    } else if (((db1C === "r") && (db2C === "s")) || ((db1C === "p") && (db2C === "r")) || ((db1C === "s") && (db2C === "p"))) {
+        db1W++;
+        db2L--;
+    }else{
+        db1T++;
+        db2T++;
+    }
+
+
+};
 
 
 //add presence for removing users on browser close
